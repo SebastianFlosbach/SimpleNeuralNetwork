de448f33356180f36b0b0965b2a31870ddc8dee7
SebastianFlosbach <sebflos@online.de>
2018-12-14 12:12:52 +0100

    Fitness modified

--- simpleNN/include/Fitness.h
+++ simpleNN/include/Fitness.h
@@ -1,8 +1,20 @@
 #pragma once
 #include <vector>
 
+typedef unsigned int Uint32;
+
 inline bool operator <( const Fitness& lhs, const Fitness& rhs ) {
-	
+	if ( lhs.size() != rhs.size() ) {
+		throw std::invalid_argument( "size of lhs and rsh are not equal" );
+	}
+
+	for ( size_t i = 0; i < lhs.size(); i++ ) {
+		if ( lhs.fitness_[i] >= rhs.fitness[i] ) {
+			return false;
+		}
+	}
+
+	return true;
 }
 inline bool operator >( const Fitness& lhs, const Fitness& rhs ) { return operator <( rhs, lhs ); }
 inline bool operator <=( const Fitness& lhs, const Fitness& rhs ) { return !operator >( lhs, rhs ); }
@@ -10,6 +22,8 @@ inline bool operator >=( const Fitness& lhs, const Fitness& rhs ) { return !oper
 
 
 struct Fitness {
+
+	Uint32 size() const { return fitness_.size(); }
 
 private:
 	std::vector<float> fitness_;
