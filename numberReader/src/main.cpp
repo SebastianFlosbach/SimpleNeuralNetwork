da129fd3b1eaf20949f393bed2a0e4f2e6df401e
SebastianFlosbach <sebflos@online.de>
2019-01-11 13:13:19 +0100

    Minor changes

--- numberReader/src/main.cpp
+++ numberReader/src/main.cpp
@@ -1,5 +1,5 @@
 #include "Network.h"
-#include "idxreader.h"
+#include "IdxReader.h"
 #include "EvolutionHandler.h"
 
 #include <iostream>
@@ -24,24 +24,7 @@ int main( int argc, char* argv[] ) {
 	
 	EvolutionHandler eHandler( std::move( net ) );
 
-	//for ( size_t i = 0; i < idxImages.size(); i++ ) {
-	//	auto idxImage = idxImages.getIdxObject<Uint8>( i );
-	//	auto idxLabel = idxLabels.getIdxObject<Uint8>( i );
-	//	
-	//	std::vector<float> input( idxImage.size() );
-	//	for ( size_t j = 0; j < idxImage.size(); j++ ) {
-	//		input[i] = static_cast<float>( idxImage.getData( j ) );
-	//	}
-
-	//	std::vector<float> output( 10 );
-	//	for ( size_t j = 0; j < 10; i++ ) {
-	//		output[j] = 0;
-	//	}
-	//	output[idxLabel.getData( 0 )];
-
-	//	TestDataPair tPair( std::move( input ), std::move( output ) );
-	//	tData.addTestDataPair( std::move( tPair ) );
-	//}
+	TestData tData( 28 * 28, 10 );
 	
 	return 0;
 }
