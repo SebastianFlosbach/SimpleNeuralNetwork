6c0dfeede9d207938a8a4364cc370ca001e601bf
SebastianFlosbach <sebflos@online.de>
2019-01-12 15:49:36 +0100

    Work on numberReader

--- numberReader/src/main.cpp
+++ numberReader/src/main.cpp
@@ -7,10 +7,24 @@
 
 typedef unsigned char Uint8;
 
+void printImage( IdxObject<Uint8>& image ) {
+	for ( size_t h = 0; h < 28; h++ ) {
+		auto row = image.getIdxObject( h );
+		for ( size_t w = 0; w < 28; w++ ) {
+			if ( row.getData( w ) > 128 ) {
+				std::cout << "X";
+			} else {
+				std::cout << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+}
+
 int main( int argc, char* argv[] ) {
 
-	IdxReader idxImages( "train-images.idx" );
-	IdxReader idxLabels( "train-labels.idx" );
+	IdxReader idxImages( "t10k-images.idx" );
+	IdxReader idxLabels( "t10k-labels.idx" );
 
 	auto net = std::make_unique<Network>();
 
@@ -24,7 +38,37 @@ int main( int argc, char* argv[] ) {
 	
 	EvolutionHandler eHandler( std::move( net ) );
 
-	TestData tData( 28 * 28, 10 );
+	auto imageData = idxImages.getIdxObject<Uint8>();
+	auto labelData = idxLabels.getIdxObject<Uint8>();
+
+	std::vector<float> inputData( 28 * 28 );
+	std::vector<float> outputData( 10 );
+	
+	auto label = labelData.getData( 0 );
+	for ( size_t l = 0; l < 10; l++ ) {
+		if ( l == label ) {
+			outputData[l] = 1;
+		} else {
+			outputData[l] = 0;
+		}
+	}
+
+	auto image = imageData.getIdxObject( i );
+	for ( size_t h = 0; h < 28; h++ ) {
+		auto row = image.getIdxObject( h );
+		for ( size_t w = 0; w < 28; w++ ) {
+			inputData[h * 28 + w] = row.getData( w );
+		}
+	}
+	
+	TestDataPair tdp( std::move( inputData ), std::move( outputData ) );
+	tData.addTestDataPair( std::move( tdp ) );
+
+	/*std::cout << std::to_string( label ) << std::endl;
+	printImage( image );*/
+
+	eHandler.addTestData( std::move( tData ) );
+	eHandler.evolveNextGeneration( 100, 0.1, 0.5 );
 	
 	return 0;
 }
